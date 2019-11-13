b72188b2ec946a27e89e7789ecb70f8370c6b934
Clarke Verdel <info@claver-webdesign.nl>
2019-11-13 15:43:28 +0100

    Update next.config.js for exporting pages & routing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,60 @@
 const path = require('path')
 const glob = require('glob')
+const fetch = require('isomorphic-unfetch')
 
 module.exports = {
+  async exportPathMap(defaultPathMap) {
+    const [postList, projectList, pageList] = await Promise.all([
+      fetch("https://cms.clarkeverdel.nl/wp-json/wp/v2/posts").then(res => res.json()),
+      fetch("https://cms.clarkeverdel.nl/wp-json/wp/v2/projects").then(res => res.json()),
+      fetch("https://cms.clarkeverdel.nl/wp-json/wp/v2/pages").then(res => res.json())
+    ])
+
+    const posts = postList.reduce(
+      (pages, post) =>
+        Object.assign({}, pages, {
+          [`/post/${post.slug}`]: {
+            page: '/post',
+            query: { slug: post.slug }
+          }
+        }),
+      {}
+    )
+
+    const projects = projectList.reduce(
+      (pages, project) =>
+        Object.assign({}, pages, {
+          [`/project/${project.slug}`]: {
+            page: '/post',
+            query: { slug: project.slug }
+          }
+        }),
+      {}
+    )
+
+    const pages = pageList.reduce(
+      (list, page) =>
+        Object.assign({}, list, {
+          [`/${page.slug}`]: {
+            page: '/post',
+            query: { slug: page.slug }
+          }
+        }),
+      {}
+    )
+
+    return Object.assign({}, posts, pages, projects, {
+      '/post': {
+        page: '/post'
+      },
+      '/project': {
+        page: '/post'
+      },
+      '/': {
+        page: '/index'
+      }
+    })
+  },
   webpack: (config, { dev }) => {
     config.module.rules.push(
       {
@@ -28,7 +81,7 @@ module.exports = {
           }
         ]
       }
-    )
+    );
     return config
   }
-}
+};
