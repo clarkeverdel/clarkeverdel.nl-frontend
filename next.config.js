b076911d272b0935d7e7e1f1672dedf702adf116
Clarke Verdel <info@claver-webdesign.nl>
2020-01-12 17:46:46 +0100

    Target serverless
    - Fixing the api route 404

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 require("dotenv").config();
 
 module.exports = withBundleAnalyzer(withTM({
+  target: 'serverless',
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     API_URL: process.env.API_URL,
