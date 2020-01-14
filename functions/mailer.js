b6eb7d52543ebe4a057edc2e4f99b3e7efa812c6
Clarke Verdel <info@claver-webdesign.nl>
2020-01-14 08:05:37 +0100

    Netlify lambda functions recreated build

---
+++ functions/mailer.js
@@ -0,0 +1,18 @@
+const mailer = require('../../mailer');
+
+module.exports = (req, res) => {
+  const { email = '', name = '', reason = '', description = '' } = req.body;
+  const emailHeading = `<p><strong>Reason of contacting: ${reason}</strong></p>`;
+  const emailBody = `<p>${description}</p>`;
+  let status = '';
+
+  mailer({ email, name, text: emailHeading + emailBody }).then((response) => {
+    status = 'Successfully sent an email via the contact form.';
+    res.send(status);
+  }).catch((error) => {
+    status = 'Failed sending the contact form';
+    res.send(status);
+  });
+
+  return status;
+};
