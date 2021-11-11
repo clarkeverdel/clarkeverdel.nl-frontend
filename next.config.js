43a49c87663dac961938b5b32b9d6c913aaa6f48
Clarke Verdel <info@claver-webdesign.nl>
2021-11-11 14:13:21 +0100

    build(): ESLint setup with v7

--- next.config.js
+++ next.config.js
@@ -14,99 +14,12 @@ module.exports = withBundleAnalyzer(withTM({
     MAIL_PASSWORD: process.env.MAIL_PASSWORD
   },
 
-  // async exportPathMap(defaultPathMap) {
-  //   const [postList, projectList, pageList] = await Promise.all([
-  //     fetch("https://cms.clarkeverdel.nl/wp-json/wp/v2/posts").then(res => res.json()),
-  //     fetch("https://cms.clarkeverdel.nl/wp-json/wp/v2/projects").then(res => res.json()),
-  //     fetch("https://cms.clarkeverdel.nl/wp-json/wp/v2/pages").then(res => res.json())
-  //   ]);
+  eslint: {
+    dirs: ['pages', 'src/components', 'src/js'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 
-  //   const posts = postList.reduce(
-  //     (pages, post) =>
-  //       Object.assign({}, pages, {
-  //         [`/post/${post.slug}`]: {
-  //           page: '/post',
-  //           query: { slug: post.slug, apiRoute: 'post' }
-  //         }
-  //       }),
-  //     {}
-  //   );
-
-  //   const projects = projectList.reduce(
-  //     (pages, project) =>
-  //       Object.assign({}, pages, {
-  //         [`/project/${project.slug}`]: {
-  //           page: '/project',
-  //           query: { slug: project.slug, apiRoute: 'project' }
-  //         }
-  //       }),
-  //     {}
-  //   );
-
-  //   const pages = pageList.reduce(
-  //     (list, page) =>
-  //       Object.assign({}, list, {
-  //         [`/${page.slug}`]: {
-  //           page: (page.slug == 'contact' ? '/contact' : '/post'),
-  //           query: { slug: page.slug, apiRoute: 'page' }
-  //         }
-  //       }),
-  //     {}
-  //   );
-
-  //   return Object.assign({}, posts, pages, projects, {
-  //     '/post': {
-  //       page: '/post'
-  //     },
-  //     '/project': {
-  //       page: '/project'
-  //     },
-  //     '/': {
-  //       page: '/index'
-  //     }
-  //   })
-  // },
   webpack: (config, options) => {
     config.module.rules.push(
-      // {
-      //   test: [/\.js$/, /\.jsx$/],
-      //   exclude: ['/node_modules/', '/.next/', '/out/'],
-      //   loader: 'eslint-loader',
-      //   enforce: 'pre',
-      //   options: {
-      //     emitWarning: true,
-      //   },
-      // },
-      // {
-      //   test: /\.(css|scss)/,
-      //   loader: 'emit-file-loader',
-      //   options: {
-      //     name: 'dist/[path][name].[ext]'
-      //   }
-      // },
-      // {
-      //   test: /\.css$/,
-      //   use: ['babel-loader', 'raw-loader', 'postcss-loader']
-      // },
-      // {
-      //   test: /\.s(a|c)ss$/,
-      //   use: ['babel-loader', 'raw-loader', 'postcss-loader',
-      //     {
-      //       loader: 'resolve-url-loader'
-      //     },
-      //     {
-      //       loader: 'sass-loader',
-      //       options: {
-      //         includePaths: ['styles', 'node_modules']
-      //           .map((d) => path.join(__dirname, d))
-      //           .map((g) => glob.sync(g))
-      //           .reduce((a, c) => a.concat(c), []),
-      //           sourceMap: true,
-      //           sourceMapContents: false
-      //       }
-      //     }
-      //   ]
-      // },
       {
         test: /\.svg$/,
         use: [{
