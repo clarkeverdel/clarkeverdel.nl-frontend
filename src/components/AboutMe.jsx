7ad45e42035d96c3b787c5de314a68087a000a14
Clarke Verdel <hello@clarkeverdel.nl>
2024-06-05 11:28:29 +0200

    feat: new image for about section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,48 +2,38 @@ import React, { Component } from 'react';
 import AnimatedButton from './AnimatedButton';
 
 // Images
-import Triangle from '../../public/static/images/TRIANGLE.svg';
-import Circle from '../../public/static/images/CIRCLE.svg';
+import Image from 'next/image';
 
-class AboutMe extends Component {
-  constructor(props) {
-    super(props);
-  }
+function AboutMe() {
+  return (
+    <section className="wrapper about-me" id="about-me">
+      <div className="about-me__wrapper">
+        <div className="about-me__header">
+          <h2 className="about-me__title h1">About me</h2>
 
-  render() {
-    return (
-      <section className="wrapper about-me" id="about-me">
-        <div className="about-me__wrapper">
-          <div className="about-me__header">
-            <h2 className="about-me__title h1">About me</h2>
-
-            <div className="about-me__photo">
-              <img className="about-me__photo__image" src="/static/images/png/ClarkAbout.png" loading="lazy"/>
-              <Triangle />
-              <Circle />
-              <img className="about-me__photo__mask" src="/static/images/png/ClarkAbout_Mask.png" loading="lazy"/>
-            </div>
-
-            <div className="about-me__subtitle">
-              Clarke Verdel
-            </div>
-            <div className="about-me__lead">
-              Creative developer
-            </div>
+          <div className="about-me__photo">
+            <Image src="/static/images/png/ClarkeAbout.png" alt="Clarke Verdel" width={1084} height={825} />
           </div>
-          <div className="about-me__content">
-            <p>
-              I&apos;m a passionate developer who likes to build high performant, awesome looking websites. I have been working as a front-end developer since 2012 and having worked on many cool projects I can say that I love my job. Helping businesses reaching their online success is my motivation in delivering awesome stuff.
-            </p>
-            <p>
-              Are you looking for someone that take things to the next level? Check out my latest projects and send me a message.
-            </p>
+
+          <div className="about-me__subtitle">
+            Clarke Verdel
+          </div>
+          <div className="about-me__lead">
+            Creative developer
           </div>
         </div>
-        <AnimatedButton color="dark" text="Contact" href="/contact"  as="/contact" className="about-me__button"/>
-      </section>
-    );
-  }
+        <div className="about-me__content">
+          <p>
+            I&apos;m a passionate developer who likes to build high performant, awesome looking websites. I have been working as a front-end developer since 2012 and having worked on many cool projects I can say that I love my job. Helping businesses reaching their online success is my motivation in delivering awesome stuff.
+          </p>
+          <p>
+            Are you looking for someone that take things to the next level? Check out my latest projects and send me a message.
+          </p>
+        </div>
+      </div>
+      <AnimatedButton color="dark" text="Contact" href="/contact" as="/contact" className="about-me__button" />
+    </section>
+  );
 }
 
 export default AboutMe;
